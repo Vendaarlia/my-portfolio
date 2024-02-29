801e6f1155875b09159b3510ec4009959a9590d0
Vendaarlia <94086273+Vendaarlia@users.noreply.github.com>
2024-02-29 14:00:30 +0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <section id="about">
         <div className='bg-heroimg bg-cover bg-center backdrop-brightness-90 flex md:flex-row flex-col items-center'>
-            <div className='w-screen h-screen p-12 flex flex-col justify-end bg-black bg-opacity-50'>
+            <div className='w-screen h-screen p-12 pb-5 flex flex-col justify-end bg-black bg-opacity-50'>
                 <div className='justify-center items-center'>
                     <h1 className=' text-4xl text text-white'>
                     SnapVision
