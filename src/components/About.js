076c40165e45abee68d858e7da030fc2a9effe0b
Vendaarlia <94086273+Vendaarlia@users.noreply.github.com>
2024-02-29 14:35:30 +0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,11 @@ function About() {
   return (
     <section id="about">
         <div className='bg-heroimg bg-cover bg-center backdrop-brightness-90 flex md:flex-row flex-col items-center'>
+<<<<<<< Updated upstream
             <div className='w-screen h-screen p-12 pb-20 flex flex-col justify-end bg-black bg-opacity-50'>
+=======
+            <div className='w-screen h-screen p-12 pb-24 flex flex-col justify-end bg-black bg-opacity-50'>
+>>>>>>> Stashed changes
                 <div className='justify-center items-center'>
                     <h1 className=' text-4xl text text-white'>
                     SnapVision
